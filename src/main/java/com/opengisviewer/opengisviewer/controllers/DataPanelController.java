da72297f25bd506a5d8a79a845616d4a909910ba
JakeIsCodingAway <jake.s.held123@gmail.com>
2023-12-10 18:51:34 -0500

    began abstracting fxml panels for different data controllers to be able to manitpulate the scenes.

---
+++ src/main/java/com/opengisviewer/opengisviewer/controllers/DataPanelController.java
@@ -0,0 +1,17 @@
+package com.opengisviewer.opengisviewer.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeView;
+
+public class DataPanelController {
+
+    @FXML
+    TextArea urlTextArea;
+    @FXML
+    Button findUrlButton;
+    @FXML
+    TreeView dataTree;
+
+}
---
+++ src/main/java/com/opengisviewer/opengisviewer/controllers/ImagePanel.java
@@ -0,0 +1,4 @@
+package com.opengisviewer.opengisviewer.controllers;
+
+public class ImagePanel {
+}
