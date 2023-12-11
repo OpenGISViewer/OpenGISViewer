0024923ff55d502bea33d3def2dd25c63592c06d
JakeIsCodingAway <jake.s.held123@gmail.com>
2023-12-10 19:05:18 -0500

    added handling for malformed url. Need to figure out what URLs will be accepted and how to validate URL response against OGC

--- src/main/java/com/opengisviewer/opengisviewer/controllers/DataPanelController.java
+++ src/main/java/com/opengisviewer/opengisviewer/controllers/DataPanelController.java
@@ -1,10 +1,18 @@
 package com.opengisviewer.opengisviewer.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeView;
+import lombok.extern.slf4j.Slf4j;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.opengisviewer.opengisviewer.controllers.ApplicationTools.generateAlert;
+
+@Slf4j
 public class DataPanelController {
 
     @FXML
@@ -13,5 +21,20 @@ public class DataPanelController {
     Button findUrlButton;
     @FXML
     TreeView dataTree;
+
+    @FXML
+    public void getUrl() {
+        String urlString = urlTextArea.getText();
+
+        try {
+            URL url = new URL(urlString);
+            // TODO: need to validate return from getting this URL get Capabilites request/ just service request returns proper capabilities document
+
+        } catch (MalformedURLException ex) { // handles no characters, or malformed URLs
+            log.error("No Valid URL provided");
+            generateAlert(Alert.AlertType.ERROR, "Unable to reach URL", "Error", "No valid URL was provided.");
+        }
+
+    }
 
 }
