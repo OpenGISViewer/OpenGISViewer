36d39c2c939929b2e1107c03e3fba3f054644fb5
JakeIsCodingAway <jake.s.held123@gmail.com>
2023-12-25 16:35:52 -0500

    documentation on utility methods i created

--- src/main/java/com/opengisviewer/opengisviewer/utils/ResourceUtils.java
+++ src/main/java/com/opengisviewer/opengisviewer/utils/ResourceUtils.java
@@ -4,6 +4,11 @@ import java.io.InputStream;
 
 public class ResourceUtils {
 
+    /**
+     * Tool to make getting resource file streams easier.
+     * @param pathFromResourceDir
+     * @return
+     */
     public static InputStream getResourceFileStream(String pathFromResourceDir){
         if(pathFromResourceDir.contains("resources")){
             pathFromResourceDir = pathFromResourceDir.replaceAll("resources", "");
