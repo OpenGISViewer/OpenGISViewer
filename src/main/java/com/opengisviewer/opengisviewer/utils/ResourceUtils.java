4137c1902fa29cde26dd6389e00805e2bf4f94f2
JakeIsCodingAway <jake.s.held123@gmail.com>
2024-01-01 15:22:28 -0500

    fixed Method invocation getFile may produce NullPointerException vulnerability in ResourceUtils.java

--- src/main/java/com/opengisviewer/opengisviewer/utils/ResourceUtils.java
+++ src/main/java/com/opengisviewer/opengisviewer/utils/ResourceUtils.java
@@ -1,6 +1,7 @@
 package com.opengisviewer.opengisviewer.utils;
 
 import java.io.InputStream;
+import java.util.Objects;
 
 public class ResourceUtils {
 
@@ -18,12 +19,18 @@ public class ResourceUtils {
         return classloader.getResourceAsStream(pathFromResourceDir);
     }
 
+
+    /**
+     * Returns the absolute path of the resource file.
+     * @param pathFromResourceDir the path of the resource file relative to the resources directory
+     * @return the absolute path of the resource file
+     */
     public static String getResourceFilePath(String pathFromResourceDir){
         if(pathFromResourceDir.contains("resources")){
             pathFromResourceDir = pathFromResourceDir.replaceAll("resources/", "");
         }
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 
-        return classloader.getResource(pathFromResourceDir).getFile();
+        return Objects.requireNonNull(classloader.getResource(pathFromResourceDir)).getFile();
     }
 }
